ec630de17dd3394fa6cde4b853a6a0a95d0ad1f9
Dan Dart <dandart@googlemail.com>
2014-07-11 22:35:58 +0100

    Start parsing

---
+++ drpl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+char token[100];
+char *stackstrings[1024];
+int sp;
+
+int main(int argc, char *argv[]) {
+	ifstream file;
+	file.open (argv[1]);
+	if (!file.is_open()) {
+		cout << "Argh! Couldn't open that file."<<endl;
+		return 1;
+	}
+	while (file >> token){
+		cout << token << endl;
+	}
+	file.close();
+	return 0;
+}
